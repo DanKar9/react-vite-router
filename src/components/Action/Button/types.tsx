7a27864207c37ac01fb9e527d0865d0091b31a6d
Danil <danil_karashchuk@mail.ru>
2025-01-18 16:25:43 +0300

    Добавление button

--- src/components/Action/Button/types.tsx
+++ src/components/Action/Button/types.tsx
@@ -1,11 +1,15 @@
+import React, {FunctionComponent} from 'react'
 
+export type ActionTag = keyof Pick<React.JSX.IntrinsicElements, 'a' | 'button' | FunctionComponent>
 
-export interface ButtonProps {
-	primary?: boolean;
-    className?: string;
-	backgroundColor?: string;
-	size?: 'small' | 'medium' | 'large';
-	label: string;
-	onClick?: () => void;
+export enum ButtonVariant {
+	primary = 'primary',
+    outline ='outline',
+	secondary = 'secondary',
+	link = 'link',
+}
 
+export enum ButtonType {
+	button = 'button',
+	submit = 'submit'
 }
