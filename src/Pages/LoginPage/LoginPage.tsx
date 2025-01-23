a2c294eb71296159f319c81591144a8a0d3e1292
Danil <danil_karashchuk@mail.ru>
2025-01-23 15:05:36 +0300

    Добавление LoginPage

---
+++ src/Pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,11 @@
+import { clsx } from "clsx";
+import { Section } from "../Containers/Section/Section.tsx";
+import { LoginForm, LoginFormProps } from "../screens/LoginForm/LoginForm.tsx";
+
+export const LoginPage = ({className, formSetting} :LoginFormProps) => {
+    return <main className={clsx{styles.container, className}}>
+    <Section>
+        <LoginForm formSetting={formSetting}></LoginForm>
+    </Section>
+    </main>
+}
